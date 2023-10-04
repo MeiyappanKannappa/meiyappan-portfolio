d3ceb818eccd4e8e0bcb7b571947775f141b1609
mrt3chie <rageshnithin.a2020@vitstudent.ac.in>
2023-10-04 21:50:32 +0530

    error page redirect

--- src/Screens/ErrorPage.jsx
+++ src/Screens/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
@@ -8,6 +9,12 @@ const ErrorPage = () => {
           <h1 class="text-[26px] md:text-[24px] md:mt-[20px] lg:text-[38px] font-bold text-[#505050]">
             404 Not Found!
           </h1>
+          <p className="text-[16px] md:text-[18px]">
+            Back to{" "}
+            <a href="/" className="text-[#1363DF]">
+              Home
+            </a>
+          </p>
         </div>
       </section>
     </main>
