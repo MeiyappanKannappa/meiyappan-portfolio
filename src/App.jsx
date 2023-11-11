cd28a9129b10262cf3da60326c86e3482dcc90c6
mrt3chie <rageshnithin.a2020@vitstudent.ac.in>
2023-11-11 12:43:21 +0530

    code reverted

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,25 @@ import About from "./Screens/About";
 import Contact from "./Screens/Contact";
 import Home from "./Screens/Home";
 import Work from "./Screens/Work";
-import { Link, Outlet } from "react-router-dom";
-
+import {
+  Routes,
+  Route,
+  Router,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <main className="md:mx-[192px] mx-9">
       <Nav />
-      <Link to="/Scratch-Final-Portfolio/">Home</Link>
-      <Link to="/Scratch-Final-Portfolio/about">About</Link>
-
-      <Outlet />
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/work" element={<Work />} />
         <Route path="/contact" element={<Contact />} />
-      </Routes> */}
+      </Routes>
     </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,29 +2,10 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./Screens/Home.jsx";
-import About from "./Screens/About.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/Scratch-Final-Portfolio/",
-    element: <App />,
-    children: [
-      {
-        path: "/Scratch-Final-Portfolio/",
-        element: <Home />,
-      },
-      {
-        path: "/Scratch-Final-Portfolio/about",
-        element: <About />,
-      },
-    ],
-  },
-]);
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
