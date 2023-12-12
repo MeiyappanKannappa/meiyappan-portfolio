5cc7ad1cd1fb354a4a38567c8bf9974fb374a5de
mrt3chie <rageshnithin.a2020@vitstudent.ac.in>
2023-12-12 20:42:28 +0530

    work component fix

--- src/data/LatestProjectData.jsx
+++ src/data/LatestProjectData.jsx
@@ -1,20 +1,20 @@
 const LatestProjectData = [
-  // {
-  //   id: 1,
-  //   link: "https://github.com/MeiyappanKannappa/node-red-contrib-ocpp-client",
-  //   imageurl: "/assets/images/project-2.png",
-  //   text: "Node Red - OCPP Client",
-  //   date: "Jul '23",
-  // },
   {
     id: 1,
+    link: "https://github.com/MeiyappanKannappa/node-red-contrib-ocpp-client",
+    imageurl: "/assets/images/project-2.png",
+    text: "Node Red - OCPP Client",
+    date: "Jul '23",
+  },
+  {
+    id: 2,
     link: "https://github.com/MeiyappanKannappa/feedback-widget",
     imageurl: "/assets/images/project-1.png",
     text: "Feedback Widget",
     date: "Feb '23",
   },
   {
-    id: 2,
+    id: 3,
     link: "https://rapidapi.com/MeiyappanKannappa/api/nlp-microsaas",
     imageurl: "/assets/images/project-3.png",
     text: "NLP-MicroSaaS",
--- src/sections/LatestProjects.jsx
+++ src/sections/LatestProjects.jsx
@@ -7,51 +7,25 @@ const LatestProjects = ({ title }) => {
       <h1 className="text-[24px] lg:text-[22px] font-bold text-[#505050]">
         {title}
       </h1>
-      <div className="mt-[10px] md:flex">
-        <div className="flex justify-center md:flex-1 md:w-full ">
-          {LatestProjectData.filter((item) => item.id % 2 === 0).map(
-            ({ id, link, imageurl, text, date }) => (
-              <div key={id} className="mb-[10px] md:mb-7">
-                <p className="text-[18px] mt-2 mb-2 lg:mt-0">
-                  {text} •{" "}
-                  <span className="italic font-poppins text-[16px]">
-                    {date}
-                  </span>
-                </p>
-                <a href={link} target="_blank">
-                  <img
-                    src={imageurl}
-                    width={"450px"}
-                    className="hidden md:flex lg:w-[510px] xl:w-[450px]"
-                  />
-                </a>
-                <img src={imageurl} className="w-full md:hidden" />
-              </div>
-            )
-          )}
-        </div>
-        <div className="flex justify-center md:flex-1 md:w-full md:ml-4 ">
-          {LatestProjectData.filter((item) => item.id % 2 !== 0).map(
-            ({ id, link, imageurl, text, date }) => (
-              <div key={id} className="mb-[10px] md:mb-7">
-                <p className="text-[18px] mt-2 mb-2 lg:mt-0">
-                  {text} •{" "}
-                  <span className="italic font-poppins text-[16px]">
-                    {date}
-                  </span>
-                </p>
-                <a href={link} target="_blank">
-                  <img
-                    src={imageurl}
-                    width={"450px"}
-                    className="hidden md:flex lg:w-[510px] xl:w-[450px]"
-                  />
-                </a>
-                <img src={imageurl} className="w-full md:hidden " />
-              </div>
-            )
-          )}
-        </div>
+      <div className="mt-[10px] md:grid md:grid-cols-2 md:gap-4">
+        {LatestProjectData.reverse().map(
+          ({ id, link, imageurl, text, date }) => (
+            <div key={id} className="mb-[10px] md:mb-7">
+              <p className="text-[18px] mt-2 mb-2 lg:mt-0">
+                {text} •{" "}
+                <span className="italic font-poppins text-[16px]">{date}</span>
+              </p>
+              <a href={link} target="_blank">
+                <img
+                  src={imageurl}
+                  width={"450px"}
+                  className="hidden md:flex lg:w-[510px] xl:w-[450px]"
+                />
+              </a>
+              <img src={imageurl} className="w-full md:hidden" />
+            </div>
+          )
+        )}
       </div>
     </div>
   );
