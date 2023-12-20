35772f38662e948d9553b545939e58945214d394
mrt3chie <rageshnithin.a2020@vitstudent.ac.in>
2023-12-20 23:08:32 +0530

    view fix

--- src/sections/Blogs.jsx
+++ src/sections/Blogs.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BlogpostData from "../data/BlogpostData";
+
+const Blog = ({ link, imageurl, text }) => {
+  const [showText, setShowText] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
+
+  useEffect(() => {
+    console.log("showText in useEffect:", showText);
+  }, [showText]);
+
+  const handleClick = () => {
+    setClickCount((prevCount) => {
+      if (prevCount === 0) {
+        setShowText(true);
+      }
+
+      if (prevCount >= 1) {
+        window.open(link, "_blank");
+        setShowText(false);
+        return 0;
+      }
+
+      return prevCount + 1;
+    });
+  };
+
+  return (
+    <div className="mb-5 md:mb-7 relative" onClick={handleClick}>
+      <div>
+        <img
+          src={imageurl}
+          width={"450px"}
+          className="object-fill border-2 border-[#505050]"
+        />
+        {showText && (
+          <div className="absolute top-0 left-0 text-center md:p-4 w-full h-full bg-[#1363DF] opacity-0 hover:opacity-90  flex justify-center items-center transition-opacity">
+            <p className="font-poppins text-[14px] md:text-[20px] text-white ">
+              {text}
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 const Blogs = ({ title }) => {
   return (
@@ -14,75 +58,17 @@ const Blogs = ({ title }) => {
               const modifiedId = item.id - 1;
               return modifiedId % 2 !== 0;
             })
-            .map(({ id, link, imageurl, text }) => (
-              <div key={id} className="mb-5 md:mb-7 relative">
-                <a href={link} target="_blank">
-                  <img
-                    src={imageurl}
-                    width={"450px"}
-                    className="object-fill border-2 border-[#505050]"
-                  />
-                  <div className="absolute top-0 left-0 text-center md:p-4 w-full h-full bg-[#1363DF] opacity-0 hover:opacity-90  flex justify-center items-center transition-opacity">
-                    <p className="font-poppins text-[14px] md:text-[20px] text-white ">
-                      {text}
-                    </p>
-                  </div>
-                </a>
-              </div>
+            .map((item) => (
+              <Blog key={item.id} {...item} />
             ))}
         </div>
-        {/* <div className="flex-1 w-full">
-          {BlogpostData.filter((item) => item.id % 2 !== 0)
-            .reverse()
-            .map(({ id, link, imageurl, text }) => (
-              <div key={id} className="mb-5 md:mb-7 ">
-                <a href={link} target="_blank">
-                  <img
-                    src={imageurl}
-                    width={"450px"}
-                    className="object-fill border-2 border-[#505050] "
-                  />
-                </a>
-              </div>
-            ))}
-        </div> */}
-        {/* <div className="flex-1 w-full ml-4">
-          {BlogpostData.filter((item) => item.id % 2 === 0)
-            .reverse()
-            .map(({ id, link, imageurl, text }) => (
-              <div
-                key={id}
-                className="mb-5 md:mb-7 hover:bg-[#1363DF] hover:opacity-50"
-              >
-                <a href={link} target="_blank">
-                  <img
-                    src={imageurl}
-                    width={"450px"}
-                    className="object-fill border-2 border-[#505050] hover:bg-[#1363DF] hover:opacity-50"
-                  />
-                </a>
-              </div>
-            ))}
-        </div> */}
+
         <div className="flex-1 w-full ml-4">
           {BlogpostData.filter((item) => {
             const modifiedId = item.id - 1;
             return modifiedId % 2 === 0;
-          }).map(({ id, link, imageurl, text }) => (
-            <div key={id} className="mb-5 md:mb-7 relative">
-              <a href={link} target="_blank">
-                <img
-                  src={imageurl}
-                  width={"450px"}
-                  className="object-fill border-2 border-[#505050]"
-                />
-                <div className="absolute top-0 left-0 text-center md:p-4 w-full h-full bg-[#1363DF] opacity-0 hover:opacity-90  flex justify-center items-center transition-opacity">
-                  <p className="font-poppins text-[14px] md:text-[20px] text-white ">
-                    {text}
-                  </p>
-                </div>
-              </a>
-            </div>
+          }).map((item) => (
+            <Blog key={item.id} {...item} />
           ))}
         </div>
       </div>
--- src/sections/RecentBlogs.jsx
+++ src/sections/RecentBlogs.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BlogpostData from "../data/BlogpostData";
+
+const Blog = ({ id, link, imageurl, text }) => {
+  const [showText, setShowText] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
+
+  useEffect(() => {
+    console.log("showText in useEffect:", showText);
+  }, [showText]);
+
+  const handleClick = () => {
+    setClickCount((prevCount) => {
+      if (prevCount === 0) {
+        setShowText(true);
+      }
+
+      if (prevCount >= 1) {
+        window.open(link, "_blank");
+        setShowText(false);
+        return 0;
+      }
+
+      return prevCount + 1;
+    });
+  };
+
+  return (
+    <div className="mb-5 md:mb-7 relative" onClick={handleClick}>
+      <div>
+        <img
+          src={imageurl}
+          className="w-[450px] h-[150px] md:h-[250px] lg:h-[350px] border-2 border-[#505050] object-contain"
+        />
+        {showText && (
+          <div className="absolute top-0 left-0 text-center md:p-4 w-full h-full bg-[#1363DF] opacity-90 flex justify-center items-center transition-opacity">
+            <p className="font-poppins text-[14px] md:text-[20px] text-white ">
+              {text}
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 const Blogs = ({ title }) => {
   return (
-    <div className="lg:flex lg:justify-between">
+    <div className="xl:flex lg:justify-between">
       <h1 className="text-[26px] font-bold text-[#505050] lg:text-[22px] mb-[10px]">
         {title}
       </h1>
@@ -14,63 +57,20 @@ const Blogs = ({ title }) => {
               const modifiedId = item.id - 1;
               return modifiedId % 2 !== 0;
             })
-            .slice(0, 2) // Only take the first 2 items
-            .map(({ id, link, imageurl, text }) => (
-              <div key={id} className="mb-5 md:mb-7 relative">
-                <a href={link} target="_blank">
-                  <img
-                    src={imageurl}
-                    className="w-[450px] h-[150px] md:h-[250px] lg:h-[350px] border-2 border-[#505050] object-contain  "
-                  />
-                  <div className="absolute top-0 left-0 text-center md:p-4 w-full h-full bg-[#1363DF] opacity-0 hover:opacity-90  flex justify-center items-center transition-opacity">
-                    <p className="font-poppins text-[14px] md:text-[20px] text-white ">
-                      {text}
-                    </p>
-                  </div>
-                </a>
-              </div>
+            .slice(0, 2)
+            .map((item) => (
+              <Blog key={item.id} {...item} />
             ))}
         </div>
-        {/* <div className="flex-1 w-full ml-4">
-          {BlogpostData.reverse()
-            .filter((item) => item.id % 2 === 0)
-            .reverse()
-            .slice(0, 2) // Only take the first 2 items
-            .map(({ id, link, imageurl, text }) => (
-              <div
-                key={id}
-                className="mb-5 md:mb-7 hover:bg-[#1363DF] hover:opacity-50"
-              >
-                <a href={link} target="_blank">
-                  <img
-                    src={imageurl}
-                    width={"450px"}
-                    className="h-[150px] md:h-[250px] lg:h-[350px] border-2 border-[#505050] object-contain hover:bg-[#1363DF] hover:opacity-50"
-                  />
-                </a>
-              </div>
-            ))}
-        </div> */}
+
         <div className="flex-1 w-full ml-4">
           {BlogpostData.filter((item) => {
             const modifiedId = item.id - 1;
             return modifiedId % 2 === 0;
           })
-            .slice(0, 2) // Only take the first 2 items
-            .map(({ id, link, imageurl, text }) => (
-              <div key={id} className="mb-5 md:mb-7 relative">
-                <a href={link} target="_blank">
-                  <img
-                    src={imageurl}
-                    className="w-[450px] h-[150px] md:h-[250px] lg:h-[350px] border-2 border-[#505050] object-contain"
-                  />
-                  <div className="absolute top-0 left-0 text-center md:p-4 w-full h-full bg-[#1363DF] opacity-0 hover:opacity-90  flex justify-center items-center transition-opacity">
-                    <p className="font-poppins text-[14px] md:text-[20px] text-white ">
-                      {text}
-                    </p>
-                  </div>
-                </a>
-              </div>
+            .slice(0, 2)
+            .map((item) => (
+              <Blog key={item.id} {...item} />
             ))}
         </div>
       </div>
